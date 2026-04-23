0fb21e4ac1e803919a8a6db56d500ad352868479
jules ruzindana <julruzindana@gmail.com>
2026-04-23 14:55:30 +0200

    added productRepository method to test trigger workflow

--- catalog-service/src/main/java/com/sivalabs/bookstore/catalog/domain/ProductRepository.java
+++ catalog-service/src/main/java/com/sivalabs/bookstore/catalog/domain/ProductRepository.java
@@ -3,8 +3,9 @@ package com.sivalabs.bookstore.catalog.domain;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-
-interface ProductRepository extends JpaRepository <ProductEntity,Long>{
+interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     Optional<ProductEntity> findByCode(String code);
+
+    Optional<ProductEntity> findByImageUrl(String imageUrl);
 
 }
