901cb3fc8d08d0009c34807b2fdc748b281112b6
jules ruzindana <julruzindana@gmail.com>
2026-04-27 09:34:48 +0200

    added order-service module and RabbitMQ configuration

---
+++ order-service/src/test/java/com/sivalabs/bookstore/orders/TestOrderServiceApplication.java
@@ -0,0 +1,10 @@
+package com.sivalabs.bookstore.orders;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestOrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(OrderServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+}
