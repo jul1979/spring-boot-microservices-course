819767a8a1d2c721905fa29834970fdfcbc8601c
jules ruzindana <julruzindana@gmail.com>
2026-05-13 12:08:28 +0200

    Implementing Notification Service

---
+++ notification-service/src/test/java/com/sivalabs/bookstore/notifications/AbstractIT.java
@@ -0,0 +1,13 @@
+package com.sivalabs.bookstore.notifications;
+
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+
+@SpringBootTest(webEnvironment = RANDOM_PORT)
+@Import(TestcontainersConfiguration.class)
+public abstract class AbstractIT {
+   
+}
---
+++ notification-service/src/test/java/com/sivalabs/bookstore/notifications/NotificationServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.sivalabs.bookstore.notifications;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class NotificationServiceApplicationTests extends AbstractIT {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
